03df1dc7b937440a328a39980a4683f62b8ea4df
Francesco Scaltrito <101185088+FrancescoScaltrito@users.noreply.github.com>
2026-03-16 16:33:07 +0100

    Commit Spring Boot vuoto

---
+++ src/test/java/com/betacom/betabooks/BetaBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.betabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
