876bbf803b5f046c486a4847b5ee61b831249173
Francesco Scaltrito <101185088+FrancescoScaltrito@users.noreply.github.com>
2026-03-16 17:41:54 +0100

    primo francesco

---
+++ src/main/java/com/betacom/betabooks/BetaBooksApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.betabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaBooksApplication.class, args);
+	}
+
+}
